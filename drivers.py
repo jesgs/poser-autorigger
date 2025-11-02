9cbcec0d4e61f850983773349719d9a61d8a19f9
Jess Green <2304490+jesgs@users.noreply.github.com>
2025-11-02 13:51:52 -0600

    Minor refactoring of for loop

--- drivers.py
+++ drivers.py
@@ -15,25 +15,27 @@ def create_and_add_drivers():
     arm_bone_chain = ['FK-Hand', 'FK-Forearm', 'FK-Shoulder', 'FK-Collar']
     for b in arm_bone_chain:
         for bone in pose_bones:
-            if b in bone.name:
-                is_left = bone.name.find('.L') != -1
+            if b not in bone.name:
+                continue
 
-                pose_bone_constraints  = bone.constraints
-                for constraint in pose_bone_constraints:
-                    if 'IK-' in constraint.name and constraint.type == 'COPY_TRANSFORMS':
-                        fcurve = constraint.driver_add("influence")
-                        driver = fcurve.driver
-                        driver.type = 'SCRIPTED'
+            is_left = bone.name.find('.L') != -1
 
-                        var = driver.variables.new()
-                        var.name = "fkik_switch"
-                        var.type = 'SINGLE_PROP'
-                        target = var.targets[0]
-                        target.id_type = 'OBJECT'
-                        target.id = armature
-                        if is_left:
-                            target.data_path = 'pose.bones["PROPERTIES"]["arms_fkik"][0]'  # Custom property path
-                        else:
-                            target.data_path = 'pose.bones["PROPERTIES"]["arms_fkik"][1]'  # Custom property path
-                        # Set driver expression
-                        driver.expression = "fkik_switch"
+            pose_bone_constraints  = bone.constraints
+            for constraint in pose_bone_constraints:
+                if 'IK-' in constraint.name and constraint.type == 'COPY_TRANSFORMS':
+                    fcurve = constraint.driver_add("influence")
+                    driver = fcurve.driver
+                    driver.type = 'SCRIPTED'
+
+                    var = driver.variables.new()
+                    var.name = "fkik_switch"
+                    var.type = 'SINGLE_PROP'
+                    target = var.targets[0]
+                    target.id_type = 'OBJECT'
+                    target.id = armature
+                    if is_left:
+                        target.data_path = 'pose.bones["PROPERTIES"]["arms_fkik"][0]'  # Custom property path
+                    else:
+                        target.data_path = 'pose.bones["PROPERTIES"]["arms_fkik"][1]'  # Custom property path
+                    # Set driver expression
+                    driver.expression = "fkik_switch"
